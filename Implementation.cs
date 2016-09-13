475af67ed59bf4bd22126d9f611588d87917bb77
skapix <iliasr@mail.ru>
2016-09-13 22:30:21 +0300

    First steps into unknown

---
+++ Implementation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang
+{
+    class Implementation
+    {
+        static public int calculate(Node left, string operation, Node right)
+        {
+            int lValue = left.getValue();
+            int rValue = right.getValue();
+            switch (operation)
+            {
+                case "+":
+                    return lValue + rValue;
+                case "-":
+                    return lValue - rValue;
+                case "*":
+                    return lValue * rValue;
+                case "/":
+                    return lValue / rValue;
+                case "<":
+                    return Convert.ToInt32(lValue < rValue);
+                case "<=":
+                    return Convert.ToInt32(lValue <= rValue);
+                case ">":
+                    return Convert.ToInt32(lValue > rValue);
+                case ">=":
+                    return Convert.ToInt32(lValue >= rValue);
+                case "=":
+                    return Convert.ToInt32(lValue == rValue);
+                case "!=":
+                    return Convert.ToInt32(lValue != rValue);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Nodes.cs
+++ Nodes.cs
@@ -5,15 +5,45 @@ using System.Text;
 
 namespace SimpleLang
 {
-
-    public class Node
-    {}
+    abstract public class Node
+    {
+        abstract public int getValue();   
+    };
 
     public class ExprNode : Node
-    {}
+    {
+        public ExprNode(int val)
+        {
+            value = val;
+        }
+        public ExprNode(string op, Node l, Node r)
+        {
+            value = null;
+            operation = op;
+            left = l;
+            right = r;
+        }
+
+        public override int getValue()
+        {
+            return value != null ? value.Value : Implementation.calculate(left, operation, right);
+        }
+
+        int? value = null;
+        string operation = null;
+        private Node left = null;
+        private Node right = null;
+
+        
+    }
 
     public class StatementNode : Node
-    {}
+    {
+        public override int getValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
 
     public class BlockNode : Node
     {
@@ -27,5 +57,9 @@ namespace SimpleLang
             // TODO
         }
 
+        public override int getValue()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
