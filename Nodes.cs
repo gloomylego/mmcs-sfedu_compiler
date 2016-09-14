846de51dc101fc9dc41b339095071a23d334d656
skapix <iliasr@mail.ru>
2016-09-15 02:05:53 +0300

    add visitor pattern

--- Nodes.cs
+++ Nodes.cs
@@ -8,11 +8,12 @@ namespace SimpleLang
     
     public enum AssignType { Assign, AssignPlus, AssignMinus, AssignMult, AssignDivide };
 
-    public class Node    
+    public abstract class Node
     {
+        public abstract void Accept(Visitor v);
     }
 
-    public class ExprNode : Node 
+    public abstract class ExprNode : Node 
     {
     }
 
@@ -20,23 +21,54 @@ namespace SimpleLang
     {
         public string Name { get; set; }
         public IdNode(string name) { Name = name; }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+        }
     }
 
     public class IntNumNode : ExprNode
     {
         public int Num { get; set; }
         public IntNumNode(int num) { Num = num; }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+        }
     }
 
     public class BoolNode : ExprNode
     {
         public bool Bool { get; set; }
         public BoolNode(bool flag) { Bool = flag; }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+        }
     }
 
-    public class StatementNode : Node 
+    public class BinaryNode : ExprNode
     {
+        public ExprNode left { get; set; }
+        public ExprNode right { get; set; }
+        public string operation { get; set; }
+        public BinaryNode(ExprNode lhs, ExprNode rhs, string op)
+        {
+            left = lhs;
+            right = rhs;
+            operation = op;
+        }
+        public override void Accept(Visitor v)
+        {
+            left.Accept(v);
+            right.Accept(v);
+            v.Visit(this);
+        }
     }
+
+
+    public abstract class StatementNode : Node
+    { }
 
     public class AssignNode : StatementNode
     {
@@ -49,6 +81,10 @@ namespace SimpleLang
             Expr = expr;
             AssOp = assop;
         }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+        }
     }
 
     public class CycleNode : StatementNode
@@ -59,6 +95,10 @@ namespace SimpleLang
         {
             Expr = expr;
             Stat = stat;
+        }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
         }
     }
 
@@ -73,6 +113,13 @@ namespace SimpleLang
         public void Add(StatementNode stat)
         {
             StList.Add(stat);
+        }
+        public override void Accept(Visitor v)
+        {
+            v.Visit(this);
+            //foreach(StatementNode sNode in StList)
+            //    sNode.Accept(v);
+            
         }
     }
 }
