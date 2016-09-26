85593749d34c23dc147edf3492c1431e2f1645b9
skapix <aweflea@gmail.com>
2016-09-26 13:14:00 +0300

    Update LinearCode.cs

--- LinearCode.cs
+++ LinearCode.cs
@@ -227,18 +227,8 @@ namespace SimpleLang
 
             moveExpressionToCode();
         }
-        public void Visit(CycleNode cycNode) //TODO: implement me
+        public void Visit(CycleNode cycNode)
         {
-            /*
-                     t1 := 1
-                l1:  t2 := t1 GT cycNode.Expr
-                     if t2 then goto l3
-                l2:  cycNode.StatementNode
-                     t1 := t1 + 1
-                     goto l1
-                l3:  noop
-            */
-
             // t1:=1
             var varIdent = new Identificator(s_constantPrefix + valueCounter++.ToString());
             code.Add(new LinearRepresentation(Operation.Assign, varIdent, new Number(1)));
@@ -284,7 +274,7 @@ namespace SimpleLang
             
             branchCondition(condition, forNode.BodyStatement, null, beforeEnd);
         }
-        public void Visit(RepUntNode ruNode)  //TODO: implement me
+        public void Visit(RepUntNode ruNode)
         {
             var beginLabel = new Label(s_labelPrefix + labelCounter++);
             code.Add(new LinearRepresentation(Operation.LabelOp, beginLabel));
